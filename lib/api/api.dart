13d42cd8effc5902c972ce5f4b5834e23a4e41ee
Martin Salazar <martin.fsv1993@gmail.com>
2021-12-10 17:48:44 -0300

    commit on "Desarrollo"

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,24 +1,30 @@
 import "dart:convert";
+import 'package:essbio_apk/models/fase_instalacion.dart';
 import "package:http/http.dart" as http;
 import 'package:flutter/material.dart';
 import '../models/mod_wkf_orden_trabajo.dart';
 
 class EssbioProvider with ChangeNotifier {
   EssbioProvider() {
-    this.fetchTask();
-  }
-
-  List<OrdenTrabajo> _ordenesTrabajo = [];
-
-  List<OrdenTrabajo> get ordenesTrabajo {
-    return [..._ordenesTrabajo];
+    this.fetchOrdenesTrabajo();
+    this.fetchFasesInstalacion();
   }
 
   final server = "http://10.0.2.2:8000";
 
+  List<OrdenTrabajo> _ordenesTrabajo = [];
+  List<OrdenTrabajo> get ordenesTrabajo {
+    return [..._ordenesTrabajo];
+  }
+
+  List<FaseInstalacion> _fasesInstalacion = [];
+  List<FaseInstalacion> get fasesInstalacion {
+    return [..._fasesInstalacion];
+  }
+
   validateLogin(String username, String password) async {}
 
-  fetchTask() async {
+  fetchOrdenesTrabajo() async {
     final url = '${server}/mod_wkf_orden_trabajo/?format=json';
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
@@ -29,19 +35,14 @@ class EssbioProvider with ChangeNotifier {
     }
   }
 
-  void updateOrden(OrdenTrabajo ordenTrabajo) async {
-    await http.put(Uri.parse('${server}/mod_wkf/'),
-        headers: {"Content-Type": "application/json"},
-        body: json.encode(ordenTrabajo));
-  }
-
-  void updateOrdenName(String name, String id) async {
-    final url = '${server}/mod_wkf/${id}/?format=json';
-    await http.delete(Uri.parse(url),
-        headers: {"Content-Type": "application/json"},
-        body: json.encode({"NOMBRE_OT": name}));
-    await http.post(Uri.parse('http://10.0.2.2:8000/mod_wkf/'),
-        headers: {"Content-Type": "application/json"},
-        body: json.encode({"NOMBRE_OT": name}));
+  fetchFasesInstalacion() async {
+    final url = '${server}/ot_fase_instalacion/?format=json';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body) as List;
+      _fasesInstalacion = data
+          .map<FaseInstalacion>((json) => FaseInstalacion.fromJson(json))
+          .toList();
+    }
   }
 }
