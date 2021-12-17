8fb2afd8bee1fe8a72a4e02854f9a0db2bcaa132
Piero Morata <piero.morata@gmail.com>
2021-12-17 20:33:31 -0300

    Se modulariza Botón de Guardado

--- lib/widgets/widgets_essbio.dart
+++ lib/widgets/widgets_essbio.dart
@@ -1,5 +1,4 @@
 import 'package:essbio_apk/api/api.dart';
-import 'package:essbio_apk/models/fase_instalacion.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:essbio_apk/login_page.dart';
@@ -271,31 +270,6 @@ class _RotuloTKFieldState extends State<RotuloTKField> {
 
 // ****************************************
 
-class GuardarDatosBitacora extends StatefulWidget {
-  FaseInstalacion faseInstalacion;
-  GuardarDatosBitacora({Key? key, required this.faseInstalacion})
-      : super(key: key);
-  @override
-  _GuardarDatosBitacoraState createState() => _GuardarDatosBitacoraState();
-}
-
-class _GuardarDatosBitacoraState extends State<GuardarDatosBitacora> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text("Los campos marcados con * son obligatorios"),
-        SizedBox(height: 10),
-        BotonGuardar(faseInstalacion: widget.faseInstalacion),
-        SizedBox(height: 20),
-      ],
-    );
-  }
-}
-
-// ****************************************
-
 class MedicionTextField extends StatefulWidget {
   @override
   _MedicionTextFieldState createState() => _MedicionTextFieldState();
@@ -354,71 +328,6 @@ class CerrarSesionFunction extends StatelessWidget {
           ),
         ),
         child: const Text('Cerrar Sesión',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    );
-  }
-}
-
-// **********BOTON GUARDAR**************
-
-class BotonGuardar extends StatelessWidget {
-  final FaseInstalacion faseInstalacion;
-  BotonGuardar({Key? key, required this.faseInstalacion}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final essbioP = Provider.of<EssbioProvider>(context);
-    Map<String, dynamic> modificacion = {
-      "ID_TIPO_STATUS": 112,
-      "COMENTARIO_INSTALACION": "Test update",
-      "ARCHIVO_ADJUNTO": "",
-      "ROTULO_TK": "",
-    };
-    return Container(
-      height: 50,
-      width: 150,
-      color: azulPrimarioEssbio,
-      child: TextButton(
-        onPressed: () {
-          print(faseInstalacion.id);
-          print(faseInstalacion.id_ot);
-          essbioP.updateFasesInstalacion(faseInstalacion, modificacion);
-          /*showDialog<String>(
-            context: context,
-            builder: (BuildContext context) => AlertDialog(
-              title: const Text('Guardar Cambios Datos OT'),
-              content: const Text(
-                  'Has realizado modificaciones en los campos. Confirma para guardar los cambios'),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () => Navigator.pop(context, 'Cancel'),
-                  child: const Text('Cancelar'),
-                ),
-                TextButton(
-                  onPressed: () => showDialog<String>(
-                    context: context,
-                    builder: (BuildContext context) => AlertDialog(
-                      title: const Text('Cambios Guardados'),
-                      content: const Text(
-                          'Los campos han sido actualizados satisfactoriamente'),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text('Volver al Workflow'),
-                        ),
-                      ],
-                    ),
-                  ),
-                  child: const Text('Confirmar'),
-                ),
-              ],
-            ),
-          );*/
-        },
-        child: const Text('Guardar cambios',
             style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
       ),
     );
