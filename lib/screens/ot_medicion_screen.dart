d43567885de7eb53d51f9f4cedd4fc28b62a2116
Piero Morata <piero.morata@gmail.com>
2022-01-05 19:29:47 -0300

    ultimos arreglos 05 de enero

--- lib/screens/ot_medicion_screen.dart
+++ lib/screens/ot_medicion_screen.dart
@@ -579,7 +579,7 @@ class _OtMedicionScreenState extends State<OtMedicionScreen> {
                     child: Container(
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
-                          color: azulPrimarioEssbio),
+                          color: celesteEssbio),
                       height: 50.0,
                       width: screenWidth * 0.8,
                       margin: EdgeInsets.symmetric(horizontal: 20),
